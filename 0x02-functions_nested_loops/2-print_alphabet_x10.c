9cb519a107c74b7e2a7cd743626d5f007c22b8dc
Nyokabi <nyokabi@example.com>
2023-03-21 11:13:37 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,9 @@
-#incude "main.h"
+#include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase
- *
- * Return: void
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
-
 void print_alphabet_x10(void)
 {
 	char ch;
