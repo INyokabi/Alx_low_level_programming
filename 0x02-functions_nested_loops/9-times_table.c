403112d708cfb707be26a3225a055bd38f96ac34
Nyokabi <nyokabi@example.com>
2023-03-21 12:30:07 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
- *
- * Return: 0
- */
-void times_table(void)
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
 {
 	times_table();
 	return (0);
 }
-
-
