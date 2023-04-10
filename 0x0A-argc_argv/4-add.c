0670e1b3697a9ca136e6669afc6a6489ef3e134a
Nyokabi <nyokabi@example.com>
2023-04-10 15:34:10 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - program that adds positive numbers
+*@argc: Argument counter
+*@argv: Argument vector
+*Return: 1 if number contains non-digits, otherwise 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int a, b, sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[a]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
