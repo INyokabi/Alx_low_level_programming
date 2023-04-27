21a793275182f7fb54cdb39aacb1a7d3ac806875
INyokabi <nyokabigatuna@gmail.com>
2023-04-27 11:57:33 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,11 @@
-extern printf
-
-section .text
-
 global    main
-
+          extern    printf
 main:
-	  mov   rdi, format
+	  mov   edi, format
 	  xor   eax, eax
 	  call  printf
-	  mov 	rax, 0
+	  mov 	eax, 0
 	  ret
-
-section .data
 
 message: db `Hello, Holberton\n`,0
 format: db "%s", 10, 0
