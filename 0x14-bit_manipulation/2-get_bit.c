dcf0514252d72436264b304863c8420b4eaf9767
INyokabi <nyokabigatuna@gmail.com>
2023-05-05 13:43:04 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
 /**
  * get_bit - function that returns the value of a bit at a given index
  * @n: number to search for
- * @index:is the index, starting from 0 of the bit you want to get 
+ * @index:is the index, starting from 0 of the bit you want to get
  *
  * Return: the value of the bit at index index
  * or -1 if an error occured
